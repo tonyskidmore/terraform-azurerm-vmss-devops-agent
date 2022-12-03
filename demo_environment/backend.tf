51e20dd3d3291320f8af5bf6f9a0bcd71eb946bd
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-12-03 18:37:47 +0000

    removing backend

--- demo_environment/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-demo-azure-devops-vmss"
-    storage_account_name = "sademovmss302300"
-    container_name       = "tfstate"
-    key                  = "000-bootstrap.tfstate"
-  }
-}
