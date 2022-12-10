2e5b8e26663e1e2ce743eac26a88319e44c4a950
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-12-10 10:56:04 +0000

    updating rules

--- demo_environment/bootstrap_update/tf/storage.tf
+++ demo_environment/bootstrap_update/tf/storage.tf
@@ -1,3 +1,7 @@
+data "http" "ifconfig" {
+  url = "https://ifconfig.me/ip"
+}
+
 resource "azurerm_storage_account" "demo-vmss" {
   name                          = "sademovmss${random_string.build_index.result}"
   resource_group_name           = azurerm_resource_group.demo-vmss.name
@@ -5,6 +9,12 @@ resource "azurerm_storage_account" "demo-vmss" {
   account_tier                  = "Standard"
   account_replication_type      = "LRS"
   public_network_access_enabled = true
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = [data.http.ifconfig.body]
+    virtual_network_subnet_ids = [azurerm_subnet.demo-vmss.id]
+  }
 
   tags = var.tags
 }
