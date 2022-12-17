990a708287f94f6803d755c2ef2887e2543c735c
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2022-12-17 15:10:59 +0000

    removing rest provider

--- demo_environment/providers.tf
+++ demo_environment/providers.tf
@@ -17,10 +17,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">=3.1.0"
     }
-    restapi = {
-      source  = "Mastercard/restapi"
-      version = ">=1.18.0"
-    }
     null = {
       version = ">= 3.2.1"
     }
@@ -30,20 +26,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-}
-
-provider "restapi" {
-  alias = "restapi_headers"
-  uri   = var.ado_org
-  debug = true
-  # write_returns_object = true
-  create_returns_object = "true"
-  # username             = "dummy"
-  # password             = var.ado_ext_pat
-
-  headers = {
-    Content-Type  = "application/json",
-    Authorization = "Basic ${base64encode(":${var.ado_ext_pat}")}"
-    # Accept        = "application/json;api-version=7.1-preview.4"
-  }
 }
