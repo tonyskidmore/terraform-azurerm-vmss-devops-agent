fdb2e42ecfce343b2b8f0e9786dd2ae1697715a5
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-04-30 20:57:30 +0100

    updtaing k8s module version

--- examples/007-aks-agents/aks/main.tf
+++ examples/007-aks-agents/aks/main.tf
@@ -30,13 +30,13 @@ data "azurerm_kubernetes_cluster" "default" {
   name                = var.cluster_name
   resource_group_name = var.resource_group_name
   depends_on = [
-    module.aks-cluster
+    module.aks
   ]
 }
 
 module "terraform-kubernetes-azure-devops-agent" {
   source      = "tonyskidmore/azure-devops-agent/kubernetes"
-  version     = "0.0.3"
+  version     = "0.0.4"
   ado_ext_pat = var.ado_ext_pat
   ado_org     = var.ado_org
 
